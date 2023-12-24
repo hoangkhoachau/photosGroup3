6694c92e235fafcbb3a9c19f33242cc6e67a424a
vtd182 <92201436+vtd182@users.noreply.github.com>
2023-12-24 23:04:48 +0700

    fix random func

--- app/src/main/java/com/example/photosGroup3/ListAdapterFeaturedPhotos.java
+++ app/src/main/java/com/example/photosGroup3/ListAdapterFeaturedPhotos.java
@@ -38,8 +38,11 @@ public class ListAdapterFeaturedPhotos extends ListAdapter{
     void randomize() {
         regenerateRandomFactor();
         imagePhotos.clear();
-        for (int i = 0; i < numItems; i++) {
-            imagePhotos.add(listAdapter.imagePhotos.get(((i + randomFactor) % listAdapter.imagePhotos.size())));
+        int size = listAdapter.imagePhotos.size();
+        if (size != 0) {
+            for (int i = 0; i < numItems; i++) {
+                imagePhotos.add(listAdapter.imagePhotos.get(size % (i + randomFactor)));
+            }
         }
         notifyDataSetChangedNotify();
     }
